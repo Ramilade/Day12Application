24cd6f6b68208fce7f93243e97406866716a2f8e
Ramilade <rami0904@stud.kea.dk>
2023-02-01 09:39:57 +0100

    Test added

--- src/main/java/sem3/day1_2/repositories/PlayerRepository.java
+++ src/main/java/sem3/day1_2/repositories/PlayerRepository.java
@@ -1,8 +1,8 @@
 package sem3.day1_2.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import sem3.day1_2.entity.Player;
 
-public interface PlayerRepository extends CrudRepository<Player,Integer> {
+public interface PlayerRepository extends JpaRepository<Player,Integer> {
 }
-
